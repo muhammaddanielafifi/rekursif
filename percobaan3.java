bfa6809c5b8140e2859ee099dca3048c20aa35d0
muhammaddanielafifi <254107020206@student.polinema.ac.id>
2025-12-03 12:07:22 +0700

    percobaan3

---
+++ percobaan3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class percobaan3 {
+    public static double hitungLaba(double saldo, int tahun) {
+        if (tahun == 0) {
+            return saldo;
+        } else {
+            return hitungLaba(saldo * 1.11, tahun - 1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double saldoAwal;
+        int tahun;
+        System.out.print("Jumlah saldo awal: ");
+        saldoAwal = sc.nextDouble();
+        System.out.print("Lamanya investasi (tahun): ");
+        tahun = sc.nextInt();
+
+        System.out.print("Jumlah saldo setelah " + tahun + " tahun: ");
+        System.out.println(percobaan3.hitungLaba(saldoAwal, tahun));
+    }
+}
